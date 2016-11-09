cbf754fd2c9755920d74e1def61f77f0ad36e3f6
adil1994 <bouktaib.adil1@gmail.com>
2016-11-09 19:45:28 +0000

    Hello World
    the start of our project
    TODOS :
    1  - index.html
    2  - press.php
    3  - login.php
    4  - add.php
    5  - backup.php

---
+++ Admin/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bouktaib
+ * Date: 11/9/2016
+ * Time: 6:43 PM
+ */
---
+++ press.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bouktaib
+ * Date: 11/9/2016
+ * Time: 6:28 PM
+ */
+
+echo 'TODO';
